dae780d66bdec1140717974584da67448c39ca14
Quentin Read <quentin.read@usda.gov>
2022-01-13 10:46:25 -0500

    export and plot intermediate results

---
+++ combine_export_metrics.R
@@ -0,0 +1,20 @@
+# GENOMIC SELECTION SUGARCANE: Combine results and write to single file to export
+# Author: Quentin Read
+# Date: 13 January 2022
+# -------------------------------------------------------------------------------
+
+library(data.table)
+library(glue)
+
+source('GS_sugarcane_2022_fns.R') 
+
+physical_traits <- c("stkwt_kg", "diam", "Brix", "Fiber", "Pol", "Sucrose", "Purity", "stalk_ha")
+economic_traits <- c("TCH", "TRS", "CRS", "TSH", "EI")
+
+metrics <- CJ(iter = 1:25, trait = c(physical_traits, economic_traits), crop_cycle = c('Plant_Cane_2017', 'Ratoon_1_2018', 'Ratoon_2_2019'))
+metrics[, file_name := with(metrics, glue('project/output/metrics_{trait}_{crop_cycle}_{iter}.csv'))]
+metrics <- metrics[file.exists(file_name)]
+metrics[, data := lapply(file_name, fread)]
+metrics <- unnest_dt(metrics, col = data, id = .(iter, trait, crop_cycle))
+
+fwrite(metrics, 'project/metrics_13jan2022.csv')

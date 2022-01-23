54ffe32801d8ac3d8a499f2925fc6510eba8b1fa
Quentin Read <quentin.read@usda.gov>
2022-01-22 20:55:59 -0500

    New results doc

--- combine_export_metrics.R
+++ combine_export_metrics.R
@@ -11,7 +11,7 @@ source('GS_sugarcane_2022_fns.R')
 physical_traits <- c("stkwt_kg", "diam", "Brix", "Fiber", "Pol", "Sucrose", "Purity", "stalk_ha")
 economic_traits <- c("TCH", "TRS", "CRS", "TSH", "EI")
 
-metrics <- CJ(iter = 1:25, trait = c(physical_traits, economic_traits), crop_cycle = c('PlantCane', 'Ratoon1', 'Ratoon2'))
+metrics <- CJ(iter = 1:5, trait = c(physical_traits, economic_traits), crop_cycle = c('PlantCane', 'Ratoon1', 'Ratoon2'))
 metrics[, file_name := with(metrics, glue('project/output/metrics_{trait}_{crop_cycle}_{iter}.csv'))]
 metrics <- metrics[file.exists(file_name)]
 metrics[, data := lapply(file_name, fread)]

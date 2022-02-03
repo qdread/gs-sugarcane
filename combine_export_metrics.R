660fdeb0ea4ad510d586ce178c15cefaef5689d6
Quentin Read <quentin.read@usda.gov>
2022-02-03 13:10:24 -0500

    Finish herit script

--- combine_export_metrics.R
+++ combine_export_metrics.R
@@ -6,15 +6,30 @@
 library(data.table)
 library(glue)
 
-source('GS_sugarcane_2022_fns.R') 
+physical_traits <- c("stkwt_kg", "diam", "Brix", "Fiber", "Pol", "Sucrose", "Purity", "stalk_ha")
+economic_traits <- c("TCH", "TRS", "CRS", "TSH", "EI")
+
+combos <- CJ(iter = 1:25, trait = c(physical_traits, economic_traits), crop_cycle = c('PlantCane', 'Ratoon1', 'Ratoon2'))
+combos[, file_name := with(combos, glue('project/output/metrics_{trait}_{crop_cycle}_{iter}.csv'))]
+combos <- combos[file.exists(file_name)]
+metrics <- combos[, fread(file_name), by = .(iter, trait, crop_cycle)]
+
+fwrite(metrics, 'project/metrics_18jan2022.csv')
+
+
+# Export observed and predicted values ------------------------------------
+
+library(data.table)
+library(glue)
 
 physical_traits <- c("stkwt_kg", "diam", "Brix", "Fiber", "Pol", "Sucrose", "Purity", "stalk_ha")
 economic_traits <- c("TCH", "TRS", "CRS", "TSH", "EI")
 
-metrics <- CJ(iter = 1:5, trait = c(physical_traits, economic_traits), crop_cycle = c('PlantCane', 'Ratoon1', 'Ratoon2'))
-metrics[, file_name := with(metrics, glue('project/output/metrics_{trait}_{crop_cycle}_{iter}.csv'))]
-metrics <- metrics[file.exists(file_name)]
-metrics[, data := lapply(file_name, fread)]
-metrics <- unnest_dt(metrics, col = data, id = .(iter, trait, crop_cycle))
+combos <- CJ(iter = 1:25, trait = c(physical_traits, economic_traits), crop_cycle = c('PlantCane', 'Ratoon1', 'Ratoon2'))
+combos[, file_name := with(combos, glue('project/output/phenotypes_{trait}_{crop_cycle}_{iter}.csv'))]
+combos <- combos[file.exists(file_name)]
+phenotypes <- combos[, fread(file_name), by = .(iter, trait, crop_cycle)]
+phenotypes <- phenotypes[, .(trait, crop_cycle, iter, model, fold, Clone, Y_obs, Y_pred)]
+setnames(phenotypes, old = 'iter', new = 'iteration')
 
-fwrite(metrics, 'project/metrics_18jan2022.csv')
+fwrite(phenotypes, 'project/observed_predicted_phenotypes_03feb2022.csv')
--- heritability_2022.R
+++ heritability_2022.R
@@ -4,13 +4,15 @@
 
 # Copy the code from GS_sugarcane_2022.R to load phenotype and genotype data
 # Then calculate heritability
+# Note: clone checks are removed before estimating the variance components, and missing values are excluded (not imputed)
 
 # Load packages -----------------------------------------------------------
 
 library(readxl)
 library(data.table)
 library(purrr)
-library(sommer) 
+library(rrBLUP) 
+library(furrr)
 
 source('GS_sugarcane_2022_fns.R') # Load needed functions
 
@@ -29,8 +31,24 @@ for (i in 1:length(phenotype_sheets)) names(phenotype_sheets[[i]])[names(phenoty
 
 phenotypes <- rbindlist(phenotype_sheets, fill = TRUE)
 
+# remove clone checks and Ratoonability
+check_IDs <- c('CP96-1252', 'CP00-1101')
+phenotypes <- phenotypes[!Clone %in% check_IDs & !crop_cycle %in% 'Ratoonability']
+
+# Find columns representing clones
 id_columns <- c("rs.", "alleles", "chrom", "pos", "strand", "assembly.", "center", "protLSID", "assayLSID", "panelLSID", "QCcode")
 clone_columns <- setdiff(names(genotypes), id_columns)
+
+# One-hot encoding of crop cycle column to use in mixed.solve() as fixed effect design matrix
+X <- dcast(data.table(id = 1:nrow(phenotypes), crop_cycle = phenotypes$crop_cycle), id ~ crop_cycle, length)
+X[, id := NULL]
+
+# One-hot encoding of clone column to use in mixed.solve()
+Z <- dcast(data.table(id = 1:nrow(phenotypes), Clone = phenotypes$Clone), id ~ Clone, length)
+Z[, id := NULL]
+
+# Sort columns of Z to match additive matrix calculated later
+Z <- Z[, ..clone_columns]
 
 # Convert SNPs to numerical format ----------------------------------------
 
@@ -49,12 +67,48 @@ geno_mat <- do.call(cbind, geno_mat) # Transpose the matrix so rows are individu
 # Set row names of genotype matrix to names of clones
 dimnames(geno_mat) <- list(clone_columns, NULL)
 
+# Get additive genetic relationship matrix. Recode to -1, 0, 1 format for rrBLUP by subtracting 1.
+A <- A.mat(geno_mat - 1, n.core=3, shrink=FALSE, return.imputed=FALSE)
 
-# Find broad-sense h2 -----------------------------------------------------
+# Loop through to calculate h2 --------------------------------------------
 
+# We need to calculate both broad sense and narrow sense heritability for each crop cycle separately and combined
 
-# Find narrow-sense h2 ----------------------------------------------------
+physical_traits <- c("stkwt_kg", "diam", "Brix", "Fiber", "Pol", "Sucrose", "Purity", "stalk_ha")
+economic_traits <- c("TCH", "TRS", "CRS", "TSH", "EI")
+crop_cycles <- c("Plant Cane_2017", "Ratoon 1_2018", "Ratoon 2_2019")
 
+# Function to get heritability from a model fit
+h2 <- function(fit) fit[['Vu']]/(fit[['Vu']] + fit[['Ve']])
 
+# Function to get narrow and broad heritability for a given trait, both with all crop cycles combined and separately
+get_all_h2 <- function(trait_to_use) {
+  message('Trait: ', trait_to_use)
+  
+  y <- phenotypes[[trait_to_use]]
+  
+  # All crop cycles combined in one model. Crop cycle fixed effect, genotype random effect
+  # Pass genotype one-hot encoded as random effects Z, crop-cycle one-hot encoded as fixed effects X
+  # Pass additive matrix A as random effects covariance matrix K for narrow sense
+  # Do not pass anything as K matrix for broad sense. It will be assumed to be the identity matrix.
+  fit_combined_narrow <- mixed.solve(y = y, Z = Z, K = A, X = X, method = 'REML')
+  fit_combined_broad <- mixed.solve(y = y, Z = Z, X = X, method = 'REML')
+  h2_combined <- data.frame(crop_cycle = 'combined', h2_narrow = h2(fit_combined_narrow), H2_broad = h2(fit_combined_broad))
+  
+  # Each crop cycle as a separate model.
+  # Pass genotype as random effects Z and covariance K as above, but do not pass any fixed effects X
+  h2_by_cycle <- map_dfr(crop_cycles, function(cycle) {
+    idx <- phenotypes$crop_cycle == cycle
+    fit_cycle_narrow <- mixed.solve(y = y[idx], Z = Z[idx, ], K = A, method = 'REML')
+    fit_cycle_broad <- mixed.solve(y = y[idx], Z = Z[idx, ], method = 'REML')
+    data.frame(crop_cycle = cycle, h2_narrow = h2(fit_cycle_narrow), H2_broad = h2(fit_cycle_broad))
+  })
+  
+  data.table(trait = trait_to_use, rbind(h2_combined, h2_by_cycle))
+}
 
+# Calculate h2 for all traits in parallel across 4 cores
+plan(multisession(workers = 4))
 
+h2_all_traits <- future_map_dfr(c(physical_traits, economic_traits), get_all_h2)
+fwrite(h2_all_traits, 'project/output/h2_all_traits.csv')
